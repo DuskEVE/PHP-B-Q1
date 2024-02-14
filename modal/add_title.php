d66e8fa3501dd430e5638b80ed4b45186f85aef7
DuskEVE <x987000@gmail.com>
2024-02-14 19:31:11 +0800

    finish add title modal

--- modal/add_title.php
+++ modal/add_title.php
@@ -3,6 +3,11 @@
     <div class="ct">
         標題區圖片: <input type="file" name="file" id="file">
     </div>
-    <div></div>
-    <div></div>
+    <div class="ct">
+        標題區替代文字: <input type="text" name="text" id="text">
+    </div>
+    <div class="ct">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
 </form>
